c0533c442f76994d9056c5a36444ac90efc9a2e3
FrancisDs98 <40763571+FrancisDs98@users.noreply.github.com>
2018-07-02 12:07:26 -0300

    Archivos 1 2 3
    Var nombre
    alert (nombre)
    asd=prompt
    /* */
    //
    ;

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,29 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	// comentario de una linea
+	// desde w3school 
+	var  nombre;
+	nombre=prompt("ingrese su nombre" ,"Ruben" );
+	alert(" su nombre es  :  "+nombre);
+	//alert("nombre");
+    /* "Comillas  " Las comillas sirven para definir texto literal que se muestra en 
+    pantalla con sus espacios si se requiere o cualquier cosa que se desee.
+    
+    */
+	//var person = prompt("Please enter your name", "Harry Potter");
+    //var = variable : Las variables son espacios en memoria y se deben nombrar
+    //var nombre   (EJEMPLO)
+
+/*
+	var person = prompt("Please enter your name", "Harry Potter");     (EJEMPLO)
+*/
+
+/* Comentarios */
+
+ 
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,24 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*
+	alert("ejercicio 3");
+	console.log("Ejercicio 3");
+*/
+//console.log("asdasd") se muestra en sources
+var nombre;
+nombre=document.getElementById('elNombre').value;
+alert (nombre);
+//.value valor del cuadro de texto
+/*document.getElementById('')		
+document.getElementById('')	
+document.getElementById('')
+document.getElementById('')
+document.getElementById('')
+document.getElementById('')*/	
+
+//	
 }
 
 
