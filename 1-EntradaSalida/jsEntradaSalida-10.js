c79bb2d52ab6b8cf7a5e102af240d406983aae47
FrancisDs98 <40763571+FrancisDs98@users.noreply.github.com>
2018-07-04 12:00:38 -0300

    entrada
    js1 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -2,7 +2,13 @@
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
+function MostrarAumento()
 {
-	
+	var importe;
+	var resultado;
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+	resultado=importe-(importe*0.25);
+		//	resultado=importe*0.4 esta mal hecho
+	document.getElementById('resultado').value=resultado;
 }
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,14 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var nombre;
+	var edad;
+	nombre=document.getElementById('elNombre').value;
+	edad=document.getElementById('laEdad').value;
+	alert("Usted se llama "+nombre+" y tiene "+edad+" años.");
+	/*
+	Para agregar las variables por texto en alert junto con un mensaje (concatenar)
+	utilizar + luego de las comillas o antes.
+	*/
 }
 
