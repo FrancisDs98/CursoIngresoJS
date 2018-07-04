4bcef3a93794195727887411784258d5138dbd0f
FrancisDs98 <40763571+FrancisDs98@users.noreply.github.com>
2018-07-04 12:01:15 -0300

    1 2
    1 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,12 @@
-function mostrar()
+function Mostrar()
 {
-  
+  var nombre;
+  var localidad;
+
+  nombre=document.getElementById('elNombre').value;
+  localidad=document.getElementById('laLocalidad').value;
+
+
+  alert("su nombre es "+nombre+" y vive en "+localidad);
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,15 @@
 
-function mostrar()
+function Mostrar()
 {
+	var base;
+	var altura;
+	var perimetro;
+	var area;
 
+	base=prompt("Ingrese la base del triangulo equilatero.");
+	perimetro=base*3
+	altura=prompt("Ahora ingrese la altura del mismo.");
+	area=(base*altura)/2;
+
+	alert("La superficie de su triangulo es "+area+" y su perimetro es "+perimetro);
 }
