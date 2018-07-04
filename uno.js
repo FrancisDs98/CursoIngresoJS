55cb165573e1fd5442d3d1f6d779290f399bc99b
FrancisDs98 <40763571+FrancisDs98@users.noreply.github.com>
2018-07-04 00:51:07 -0300

    parcial parte 1
    parcial

---
+++ uno.js
@@ -0,0 +1,12 @@
+
+function mostrar()
+{
+var ancho;
+var largo;
+var perimetro;
+perimetro = ancho + largo * 2;
+ancho=prompt("Indique el ancho del rectangulo :", "20");
+largo=prompt("Ahora indique el largo del rectangulo :", "10");
+alert(perimetro);
+
+}
