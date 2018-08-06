ca5fdd315934bd3d41fd75eb931aa9993160867a
FrancisDs98 <40763571+FrancisDs98@users.noreply.github.com>
2018-08-06 11:32:30 -0300

    for
    for

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numerosAnteriores;
 
+	numeroIngresado=prompt("ingrese numero");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	for(numerosAnteriores=numeroIngresado-1 ; numerosAnteriores>1; numerosAnteriores--)
+	{
+		
+		if(numeroIngresado%numerosAnteriores==0)
+		{
+			console.log(numerosAnteriores);
+		}
+	}
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,34 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numerosAnteriores;
+	var numeroRecorrido;
+	var esPrimo;
 
+	numeroIngresado=prompt("ingrese numero");
+	numeroIngresado=parseInt(numeroIngresado);
 
+	for(rumeroRecorrido=numeroIngresado ; numeroRecorrido > 1 ; numeroRecorrido--)
+	{
+		esPrimo = 0;
+		for(numerosAnteriores = 2; numerosAnteriores < numeroRecorrido ; numerosAnteriores++)
+		{
+		
+			if(numeroRecorrido%numerosAnteriores==0)
+			{
+				esPrimo = 1;
+				break;
+			}
+		}
+		if(esPrimo==0){
+		console.log(numeroRecorrido + " Es un numero primo");
+		}
+	}
+	
+
+	/*else{
+				console.log(numeroIngresado + " No es un numero primo");
+		}*/
 
 
 }//FIN DE LA FUNCIÓN
